f6053571e420710d156e70fade59fac5e1696830
John Groenlund <john.gronlund@me.com>
2017-02-20 20:04:38 +0100

    Adds SplashScreenViewController

---
+++ NoNotif/SplashScreenViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  NoNotif
+//
+//  Created by John Grönlund on 09/12/2016.
+//  Copyright © 2016 John Groenlund. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController
+{
+    
+}
+
