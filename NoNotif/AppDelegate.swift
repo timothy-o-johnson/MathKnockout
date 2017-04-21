25636873bfc05f91d57d5ab543102f872b928450
John Groenlund <john.gronlund@me.com>
2017-04-22 00:58:25 +0200

    Add Crashlytics to iOS app

--- NoNotif/AppDelegate.swift
+++ NoNotif/AppDelegate.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Fabric
+import Crashlytics
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
     {
+        Fabric.with([Crashlytics.self])
+        
 //        engine.resetGameToBeKOedAllButOne()
         
 //        engine.punches = [
